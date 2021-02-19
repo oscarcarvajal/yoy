272a45b5de7a34dd10002f6ff1b9f2bf7c35cfc6
Oscar <oscarcarcarvajal@outlook.es>
2021-02-18 22:34:37 -0800

    Finish About us page

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use((req, res, next) => {
     const year = new Date();
 
     res.locals.actualYear = year.getFullYear();
+    res.locals.nombresitio = "Yoy"; 
     return next();
 });
 
--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,27 @@ const router = express.Router();
 
 
 router.get('/', (req, res) =>{
-    res.render('home');
+    res.render('home', {
+        page: 'Home'
+    });
 })
 router.get('/about', (req, res) =>{
-    res.render('about');
+    res.render('about', {
+        page: 'About US'
+    });
 })
+router.get('/contact', (req, res) =>{
+    res.render('contact', {
+        page: 'Contact'
+    });
+})
+
+router.get('/testimonials', (req, res) =>{
+    res.render('testimonials', {
+        page: 'Testimonials'
+    });
+})
+
 
 
 export default router;
